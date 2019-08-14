22e866a6259a189ef9f94e5e1b62679bbace27a1
Urbanovich Dzmitry <urbanovich.mslo@gmail.com>
2019-08-14 22:42:34 +0300

    the test work has been finished

--- index.php
+++ index.php
@@ -13,4 +13,16 @@ $posts = Post::getPostsByIdUser(1);
 
 if (!empty($posts)) {
     $result = ParserHtml::parse($posts[0]['content']);
+
+    $templateFile = dirname(__FILE__) . '/template.html';
+
+    if (file_exists($templateFile)) {
+        $template = file_get_contents($templateFile);
+        $template = str_replace('{{LEFT}}', $result['left'], $template);
+        $template = str_replace('{{RIGHT}}', $result['right'], $template);
+    } else {
+        throw new Exception('template.html file does not found.');
+    }
+
+    echo $template;
 }
--- src/Helpers/ParserHtml.php
+++ src/Helpers/ParserHtml.php
@@ -10,7 +10,38 @@ class ParserHtml
         $dom = new \DOMDocument();
         $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
 
-        /*$finder = new \DomXPath($dom);
-        $nodes = $finder->query('//div[@class="frame-image"]');*/
+        $finder = new \DomXPath($dom);
+        $nodes = $finder->query('//body/*');
+        $images = $finder->query('//img');
+        $movedImage = null;
+        foreach ($images as $image) {
+            $movedImage = $image;
+            $image->parentNode->removeChild($image);
+        }
+
+        $middle = (int)($nodes->count() / 2);
+        $leftBlock = new \DOMDocument();
+        $rightBlock = new \DOMDocument();
+        foreach ($nodes as $key => $node) {
+            if ($node instanceof \DOMElement) {
+                if ($key <= $middle) {
+                    $node = $leftBlock->importNode($node, true);
+                    $leftBlock->appendChild($node);
+                } else {
+
+                    if (($key - 1) == $middle && !is_null($movedImage)) {
+                        $image = $rightBlock->importNode($movedImage, true);
+                        $rightBlock->appendChild($image);
+                    }
+                    $node = $rightBlock->importNode($node, true);
+                    $rightBlock->appendChild($node);
+                }
+            }
+        }
+
+        return [
+            'left' => $leftBlock->saveHTML(),
+            'right' => $rightBlock->saveHTML(),
+        ];
     }
 }
